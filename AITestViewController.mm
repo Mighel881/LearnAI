958c086a3d42a6a1b3c8c411dd0fdfedd088407b
Tommy Muir <31448720+Muirey03@users.noreply.github.com>
2020-06-17 11:10:20 +0100

    Move file loading to BinaryHelper.mm

--- AITestViewController.mm
+++ AITestViewController.mm
@@ -3,46 +3,7 @@
 #import "AIDrawCell.h"
 #import "AIButtonsCell.h"
 #import "UIImage+Pixels.h"
-#include <fstream>
-
-std::vector<Eigen::MatrixXf> loadWeights(std::string filePath)
-{
-	std::vector<Eigen::MatrixXf> ret;
-
-	std::ifstream f(filePath);
-	std::string line;
-	while (std::getline(f, line))
-	{
-		unsigned rows;
-		unsigned cols;
-		Eigen::MatrixXf w;
-
-		std::istringstream iss(line);
-		float a;
-		for (int i = 0; (iss >> a); i++)
-		{
-			if (i == 0)
-			{
-				rows = a;
-			}
-			else if (i == 1)
-			{
-				cols = a;
-				w.resize(rows, cols);
-			}
-			else
-				w.data()[i - 2] = a;
-			char c;
-			iss >> c;
-			if (c != ',') break;
-		}
-
-		ret.push_back(w);
-	}
-
-	f.close();
-	return ret;
-}
+#import "BinaryHelper.h"
 
 @implementation AITestViewController
 {
---
+++ BinaryHelper.h
@@ -0,0 +1,5 @@
+#include <vector>
+#include <Eigen/Sparse>
+#include <string>
+
+std::vector<Eigen::MatrixXf> loadWeights(std::string filePath);
